af9fff9cb65d559e393a5dbb220b96f305ab1417
Nikita85 <nikita.goloshchapov@easternpeak.com>
2014-08-07 18:27:24 +0300

    stage

---
+++ js/1.js
@@ -0,0 +1,11 @@
+$(function(){
+        $('.bxslider').bxSlider({
+        minSlides: 2,
+        maxSlides: 2,
+        auto: true,
+        speed: 1000,
+        pause: 5000,
+        controls: false,
+        slideWidth: 1100
+    });
+ });
---
+++ js/2.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 4000,
+    minSlides: 6,
+    maxSlides: 3,
+    slideMargin: 10
+  });
+});
