879d1e5c84bb95e3e7018eb7307ada227a60a23b
Goloshchapov Nikita <gaboni@bigmir.net>
2014-08-10 17:28:28 +0300

    bug fix

--- js/2.js
+++ js/2.js
@@ -1,8 +1,41 @@
-$(document).ready(function(){
-  $('.slider-part').bxSlider({
-    slideWidth: 4000,
-    minSlides: 6,
-    maxSlides: 6,
-    slideMargin: 10
-  });
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        controlNav: true,
+        itemWidth: 150,
+        itemMargin: 5,
+        minItems: 2,
+        maxItems: 6
+    });
 });
+
+
+$(document).ready(function() {
+
+    var owl = $("#owl-demo");
+
+    owl.owlCarousel({
+        items : 6, //10 items above 1000px browser width
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+    // Custom Navigation Events
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    })
+    $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+        owl.trigger('owl.stop');
+    })
+
+});
+
---
+++ js/3.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1
+});
+});
+$(document).ready(function(){
+    $('.your-class2').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1
+    });
+});
